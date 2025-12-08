1a7e84e89ab9661029d445f70c44dedbb8833a5c
siru129 <sirusiru1209@gmail.com>
2025-12-08 12:50:46 +0900

    장르목록/장르별 목록 생성

--- final/finalproject/src/components/Content.jsx
+++ final/finalproject/src/components/Content.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Test from "./Contents/Test";
+import SearchAndSave from "./Contents/SearchAndSave";
 import MemberJoin from "./member/MemberJoin";
 import MemberJoinFinish from "./member/MemberJoinFinish";
 import MemberLogin from "./member/MemberLogin";
 import ReviewWrite from "./review/ReviewWrite";
 import ReviewSearch from "./review/ReviewSearch";
+import GenreList from "./Contents/GenreList";
+import ContentsListByGenre from "./Contents/ContentsListByGenre";
 
 export default function Content() {
     return (<>
@@ -15,17 +18,24 @@ export default function Content() {
 
                 {/* 분할된 화면의 주소를 지정하는 영역 (path=주소 , element=화면) */}
                 <Routes>
+                    
                     {/* contents */}
                     <Route path="/contents/test" element={<Test/>}></Route>
                     <Route path="/contents/test2" element={<SearchAndSave/>}></Route>
+                    <Route path="/contents/genreList" element={<GenreList/>}></Route>
+                    <Route path="/contents/listByGenre/:genreName" element={<ContentsListByGenre/>}></Route>
+                    
 
-                   {/* 회원 페이지 */}
-                        <Route path="/member/join" element={<MemberJoin/>}></Route>
-                        <Route path="/member/joinFinish" element={<MemberJoinFinish/>}></Route>
-                        <Route path="/member/login" element={<MemberLogin/>}></Route>
+                    {/* 회원 페이지 */}
+                    <Route path="/member/join" element={<MemberJoin/>}></Route>
+                    <Route path="/member/joinFinish" element={<MemberJoinFinish/>}></Route>
+                    <Route path="/member/login" element={<MemberLogin/>}></Route>
+                    
+                    
                     {/* 리뷰 페이지 */}
-                        <Route path="/review/insert" element={<ReviewWrite/>}></Route>
-                        <Route path="/review/search" element={<ReviewSearch/>}></Route>
+                    <Route path="/review/insert" element={<ReviewWrite/>}></Route>
+                    <Route path="/review/search" element={<ReviewSearch/>}></Route>
+
                 </Routes>
             </div>
         </div>
